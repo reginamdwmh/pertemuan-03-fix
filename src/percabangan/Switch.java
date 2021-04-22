1bb84a30964c966a4ab1095bd493bc37a78ef69d
reginamdwmh <reginamudawamah@gmail.com>
2021-04-22 21:03:37 +0800

    Percabangan Switch dosen dan mahasiswa

--- src/percabangan/Switch.java
+++ src/percabangan/Switch.java
@@ -2,17 +2,17 @@ package percabangan;
 
 public class Switch {
     public static void main(String[] args) {
-        char nilai='D'
+        char nilai='D';
         switch (nilai){
-            case 'A'
+            case 'A':
                 System.out.println("Mhs   : \"Terima kasih pak\"");
                 System.out.println("Dosen :\"Selamat ya !!\"");
                 break;
-            case 'B'
+            case 'B':
                 System.out.println("Mhs   : \"Kenapa ulun kada dapat A, Pak?\"");
                 System.out.println("Dosen : \"!@#$%\"");
                 break;
-            case 'C'
+            case 'C':
                 System.out.println("Mhs   : \"Ulun turun pul pak ai, tugas pul jua\"");
                 System.out.println("Dosen : \"Tapi bisa lah menjawab ujian?\"");
                 System.out.println("Mhs   : \"Hihihi..\"");
