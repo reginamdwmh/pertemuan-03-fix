b4dbcb007bf03f66d6240f45e4ca4e17d8493cf1
reginamdwmh <reginamudawamah@gmail.com>
2021-04-22 20:13:10 +0800

    Operator Binary Aritmatika

--- src/operator/OperatorBinary.java
+++ src/operator/OperatorBinary.java
@@ -5,4 +5,13 @@ public class OperatorBinary {
     String nama = "Regina Mudawamah";
     int jumlasSks = 24;
     double indeksPrestasiKumulatif = 4.00;
+
+    public static void main(String[] args) {
+        System.out.println(9+3);
+        System.out.println(9-3);
+        System.out.println(9*3);
+        System.out.println(9/3);
+        System.out.println(9%3);
+        System.out.println(9%2);
+    }
 }
